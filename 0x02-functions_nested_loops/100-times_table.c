6af78ef6b2b809f0551f4340eadeafd067f70df5
simozex <mouhalamohammed55@gmail.com>
2023-06-21 07:50:27 -0700

    hey

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,29 @@
 #include "main.h"
-	/**
-	 * 	 * print_times_table - prints the n times table, starting with 0
-	 * 	 	 * @n: number of the times table
-	 * 	 	 	 */
-	void print_times_table(int n)
+#include <stdio.h>
+/**
+ *  
+ *  print_times_table - hieheieei
+ *  @n: is amonster
+ *  */
+
+void print_times_table(int n)
+{
+	int i, j;
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
 		{
-					int i, j, k;
+			for (j = 0; j <= n; j++)
+			{
+				if (j != 0)
+					printf("%3d", i * j);
+				else
+					printf("0");
 
-
-							if (n >= 0 && n <= 15)
-										{
-														for (i = 0; i <= n; i++)
-																		{
-																							for (j = 0; j <= n; j++)
-																												{
-																																		k = j * i;
-																																							if (j == 0)
-																																													{
-																																																				_putchar(k + '0');
-																																																									} else if (k < 10 && j != 0)
-																																																															{
-																																																																						_putchar(',');
-																																																																												_putchar(' ');
-																																																																																		_putchar(' ');
-																																																																																								_putchar(' ');
-																																																																																														_putchar(k + '0');
-																																																																																																			} else if (k >= 10 && k < 100)
-																																																																																																									{
-																																																																																																																_putchar(',');
-																																																																																																																						_putchar(' ');
-																																																																																																																												_putchar(' ');
-																																																																																																																																		_putchar((k / 10) + '0');
-																																																																																																																																								_putchar((k % 10) + '0');
-																																																																																																																																													} else if (k >= 100)
-																																																																																																																																																			{
-																																																																																																																																																										_putchar(',');
-																																																																																																																																																																_putchar(' ');
-																																																																																																																																																																						_putchar((k / 100) + '0');
-																																																																																																																																																																												_putchar(((k / 10) % 10) + '0');
-																																																																																																																																																																																		_putchar((k % 10) + '0');
-																																																																																																																																																																																							}
-																																																																																																																																																	}
-																											_putchar('\n');
-																														}
-																}
-								}
-
+				if (j != n)
+					printf(", ");
+			}
+			printf("\n");
+		}
+	}
+}
