4ada0aaab9f66657fc9bd8e4167c4b959299b795
simozex <mouhalamohammed55@gmail.com>
2023-06-21 08:12:27 -0700

    hey

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+/**  
+ *  main - ezazae
+ *  Return: 0 if it
+ */
+int main(void)
+{
+	int k;
+	for (k = 2; k < 52; k++)
+	{
+		int i;
+		unsigned long a = 0;
+		unsigned long b = 1;
+		unsigned long sum = 0;
+
+		for (i = 2; i <= k; i++)
+		{
+			sum = a + b;
+			a = b;
+			b = sum;
+		}
+
+		printf("%ld", sum);
+		if (k != 51)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
