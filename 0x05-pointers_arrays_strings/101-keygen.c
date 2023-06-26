5a3554088074359b8960bf9feb55218a629142d1
simozex <mouhalamohammed55@gmail.com>
2023-06-26 09:50:45 -0700

    hey

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random passwords 101-crackme
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int sum;
+	char ran;
+	srand(time(NULL));
+	while (sum <= 2772)
+	{
+		ran = rand() % 128;
+		sum += ran;
+		putchar(ran);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
