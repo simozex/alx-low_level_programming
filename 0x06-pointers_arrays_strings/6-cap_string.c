b92bdd56979df005e960d94c61d1d3cd2e592c62
simozex <mouhalamohammed55@gmail.com>
2023-06-29 11:22:56 -0700

    hello world!

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * *cap_string ->  Capitalizes all words of string
+ *
+ * @str: Input String
+ *
+ * Return: String
+ */
+
+char *cap_string(char *str)
+{
+	int	i;
+	int	j;
+	char	wsp[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i - 1] == wsp[j - 1])
+			{
+				if (str[i] >= 'a' && str[i] <= 'z')
+				{
+					str[i] -= 32;
+				}
+			}
+		}
+	}
+
+	return (str);
+}
