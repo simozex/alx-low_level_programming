dd7d4e91da843ca43c564fbd17b2f742b30d9510
mohammed mouhala <133163771+simozex@users.noreply.github.com>
2023-07-08 02:57:50 +0200

    Update 6-abs.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of a given integer.
+ * @n: fo _abs input
+ *
+ * Return: always stays n success
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	return (n);
+}
