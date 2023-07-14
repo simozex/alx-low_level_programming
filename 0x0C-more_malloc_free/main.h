ff0b21a1f5c68e4104bd048e45ea75e83495cb79
simozex <mouhalamohammed55@gmail.com>
2023-07-13 17:14:48 -0700

    Trustno one

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,6 +8,5 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int main(int argc, char *argv[]);
 
 #endif
