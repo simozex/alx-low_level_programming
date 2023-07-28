ca82cc157ef6ab653fadbbf68dc5da69c1583879
simozex <mouhalamohammed55@gmail.com>
2023-07-27 19:35:20 -0700

    linked

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements
+ * @h : the first argument
+ * Return: size
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+	const list_t *link;
+
+	link = h;
+	while (link != NULL)
+	{
+		count++;
+		link = link->next;
+	}
+	return (count);
+}
