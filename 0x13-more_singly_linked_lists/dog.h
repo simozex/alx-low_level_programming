c17a8ebf291f84e3f917728f0d28fe01a9885caa
simozex <mouhalamohammed55@gmail.com>
2023-07-31 13:39:12 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/dog.h
+++ 0x13-more_singly_linked_lists/dog.h
@@ -1,10 +1,12 @@
+#ifndef DOG_H
+#define DOG_H
 #include <stdio.h>
 /**
- * struct dog - structure to define a dog
- * @name: dog's name
- * @owner: dog's owner
- * @age: dog's age
- * Description: A struct that stores the name, the owner and the age of a dog.
+ *struct dog - structure to define a dog
+ *@name: dog's name
+ *@owner: dog's owner
+ *@age: dog's age
+ *Description: A struct that stores the name, the owner and the age of a dog.
  */
 struct dog
 {
@@ -12,3 +14,4 @@ char *name;
 char *owner;
 float age;
 };
+#endif
